02309707cf05833a63a5e2dcbb9cd9e9cd5939b7
Stefan Spijkerman <stefan@Stefans-MacBook-Pro.local>
2021-04-12 18:20:25 +0200

    improvement on caroussel transition

--- JS/caroussel.js
+++ JS/caroussel.js
@@ -1,14 +1,17 @@
 window.addEventListener('load', function () {
     const track = document.querySelector('.caroussel__track');
     const slides = Array.from(track.children);
-
     const nextButton = document.querySelector('.caroussel__button--right');
     const prevButton = document.querySelector('.caroussel__button--left');
 
     const dotsNav = document.querySelector('.caroussel__nav');
     const dots = Array.from(dotsNav.children);
 
-    var styleLeftOffset = 0;
+    const slideWidth= slides[0].getBoundingClientRect().width;
+    console.log(slideWidth);
+
+    slides[0].style.left = 0;
+    slides[1].style.left = 1000 + 'px';
 
     for (i = 0; i < slides.length; i++) {
         if (i == 0) {
@@ -19,37 +22,24 @@ window.addEventListener('load', function () {
         }
         slides[i].style.left = offset + 'px';
     }
-
-    const moveToSlide = (track, currentSlide, targetSlide, direction) => {
+    const moveToSlide = (track, currentSlide, targetSlide) => {
+        track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
         currentSlide.classList.remove('current-slide');
         targetSlide.classList.add('current-slide');
-
-        if (direction == 'left') {
-            styleLeftOffset += targetSlide.children[0].width;
-            track.style.left = styleLeftOffset + 'px';
-            console.log(styleLeftOffset);
-        }
-        else {
-            styleLeftOffset -= targetSlide.children[0].width;
-            track.style.left = styleLeftOffset + 'px';
-            console.log(styleLeftOffset);
-        }
     }
 
-    nextButton.addEventListener('click', e => {
+    prevButton.addEventListener('click', e =>{
+        const currentSlide = track.querySelector('.current-slide');
+        const prevSlide = currentSlide.previousElementSibling;
 
-        const currentSlide = track.querySelector('.current-slide')
+        moveToSlide(track, currentSlide,prevSlide);
+    })
+
+    nextButton.addEventListener('click', e =>{
+        const currentSlide = track.querySelector('.current-slide');
         const nextSlide = currentSlide.nextElementSibling;
-        moveToSlide(track, currentSlide, nextSlide, 'right');
 
+        moveToSlide(track, currentSlide,nextSlide);
     })
-
-    prevButton.addEventListener('click', e => {
-        const currentSlide = track.querySelector('.current-slide')
-        const previousSlide = currentSlide.previousElementSibling;
-        moveToSlide(track, currentSlide, previousSlide, 'left');
-
-    })
-
 });
 
